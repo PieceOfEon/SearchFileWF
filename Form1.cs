306b033c3d3703d43e6039a63f1293365e8bce63
PieceOfEon <danilskripka1996@gmail.com>
2022-11-08 20:00:49 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _123
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 F = new Form2();
+
+            F.Show();
+            //System.Drawing.Drawing2D.GraphicsPath Form_Path = new System.Drawing.Drawing2D.GraphicsPath();
+            //GraphicsPath myPath = new GraphicsPath();
+            //Form_Path.AddPie(0, 0, 250, 300, -45, 90);
+            //Pen myPen = new Pen(Color.Black, 2);
+            //Graphics.DrawPath(myPen, Form_Path);
+
+
+            //Region Form_Region = new Region(Form_Path);
+            //F.Region = Form_Region;
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _123
+{
+    public partial class Form2 : Form
+    {
+        public string name;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 F2 = new Form2();
+            F2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            string name1="*.";
+            name1 += textBox1.Text;
+            name1 += "*";
+            //name = textBox1.Text;
+            string directory = @"D:\";
+            try
+            {
+                foreach (var fi in new DirectoryInfo(directory).EnumerateFiles(name1, SearchOption.AllDirectories))
+                {
+                    //MessageBox.Show(fi.FullName);
+                    label2.Text += fi.FullName+"\n";
+
+                }
+            }
+            catch (Exception) { }
+            
+        }
+    }
+}
